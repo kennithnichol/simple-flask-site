3369de7aacd76ec5a18d25857efe534bd9aa4bf1
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2025-07-19 00:33:11 -0700

    record form entries with form submission endpoint

--- server.py
+++ server.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
-@app.route('/blog')
-def blog_index():
-    return 'These are my blog entries'
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database:
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou')
+    else:
+        return 'Something went wrong. Try again'
 
 @app.route('/favicon.ico')
 def favicon():
